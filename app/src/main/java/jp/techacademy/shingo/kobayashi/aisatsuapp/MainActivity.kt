5cce65fc999d9a7e9ddb10a28efb15299362c02b
Nyannsuke01 <shingo_373_100_10ve@yahoo.co.jp>
2021-05-06 18:01:39 +0900

    Refactor code

--- app/src/main/java/jp/techacademy/shingo/kobayashi/aisatsuapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/shingo/kobayashi/aisatsuapp/MainActivity.kt
@@ -4,18 +4,15 @@ import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
+//import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button1.setOnClickListener(this)
-    }
-    override fun onClick(v: View) {
-        if (v.id == R.id.button1) {
+        button1.setOnClickListener{
             showTimePickerDialog()
         }
     }
